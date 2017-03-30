564b20c91ba69ff910d2d6904a6cbd9d1981cb11
showmen15 <showmen15@poczta.onet.pl>
2017-03-30 13:18:50 +0200

    lokalizacja mapy

--- visualization/src/main/java/pl/agh/capo/simulation/visualization/MazeVisualizer.java
+++ visualization/src/main/java/pl/agh/capo/simulation/visualization/MazeVisualizer.java
@@ -52,10 +52,10 @@ public class MazeVisualizer extends JFrame implements StateReceivedCallback {
 		// File("D:\\Desktop\\Mapy\\ok\\OtwartaPrzestrzeñ5x5m.roson")),
 		// MazeMap.class);
 
-		mazeMap = new Gson().fromJson(new FileReader(new File("\\\\dsview.pcoip.ki.agh.edu.pl\\Biblioteki-Pracownicy$\\szsz\\Desktop\\Map.json")), MazeMap.class);
-		//mazeMap = new Gson().fromJson(new FileReader(new File("D:\\Desktop\\Map.json")), MazeMap.class);
+		//mazeMap = new Gson().fromJson(new FileReader(new File("\\\\dsview.pcoip.ki.agh.edu.pl\\Biblioteki-Pracownicy$\\szsz\\Desktop\\Map.json")), MazeMap.class);
+		mazeMap = new Gson().fromJson(new FileReader(new File("D:\\Desktop\\Map.json")), MazeMap.class);
 
-		mazePanel.setMaze(mazeMap);
+		//mazePanel.setMaze(mazeMap);
 		
 		File robotConfig = new File("\\\\dsview.pcoip.ki.agh.edu.pl\\Biblioteki-Pracownicy$\\szsz\\Desktop\\Config.csv");
 		//File robotConfig = new File("D:\\Desktop\\Config.csv");

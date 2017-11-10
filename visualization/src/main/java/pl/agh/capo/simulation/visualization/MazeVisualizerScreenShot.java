fcbcbf2e86fff9ed0657aa2ee25afa0f69610ff8
szsz <showmen15@poczta.onet.pl>
2017-11-10 09:17:15 +0100

    visualizatior

--- visualization/src/main/java/pl/agh/capo/simulation/visualization/MazeVisualizerScreenShot.java
+++ visualization/src/main/java/pl/agh/capo/simulation/visualization/MazeVisualizerScreenShot.java
@@ -3,12 +3,16 @@ package pl.agh.capo.simulation.visualization;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 
 import pl.agh.capo.configure.ConnectMSSQLServer;
 import pl.agh.capo.configure.RunAllgorytmConfigureSimulation;
 import pl.agh.capo.configure.TaskConfig;
 import pl.agh.capo.simulation.robot.RobotManager;
+import pl.agh.capo.utilities.state.Location;
+import pl.agh.capo.utilities.state.Point;
 import pl.agh.capo.utilities.state.State;
+import pl.agh.capo.utilities.state.Velocity;
 
 public class MazeVisualizerScreenShot {
 
@@ -44,13 +48,46 @@ public class MazeVisualizerScreenShot {
 				mazeVisualizer.IS_MAX_MODE = true;
 				mazeVisualizer.open(mapPath, sCaseName);
 				
-				RobotManager robotManager = new RobotManager(robotConfig, mazeMap);
+				ArrayList<State>  result = getConfigurationList(configure.ConfigFile);
+				
+				for (int i = 0; i < result.size(); i++) {
+					mazeVisualizer.handle(result.get(i));
+				}
+						
+				
+				
+			//	RobotManager robotManager = new RobotManager(robotConfig, mazeMap);
 				
 			//	State[] tempLog = log.GetVisualizeRobotCaseSimulation(configure.ID_Case,configure.ID_Trials);
 				
 			//	StateCollectorVisualizer vis = new StateCollectorVisualizer(mazeVisualizer,tempLog);
 				//System.exit(1);		
 		
+	}
+	
+	public static ArrayList<State> getConfigurationList(String sRobotConfig)
+	{
+		ArrayList<State> result = new ArrayList<State>();
+		
+	    String[] robotData = sRobotConfig.split("\n");
+	    
+	    for (int i = 0; i < robotData.length; i++) 
+	    {
+			String[] temp = robotData[i].split(";");
+	    	int idRobot = Integer.parseInt(temp[0]);	
+	    	double locX = Double.parseDouble(temp[3]) ;
+	    	double locY = Double.parseDouble(temp[4]);
+	    	double locAlfa = Double.parseDouble(temp[1]);
+			
+			
+			State item = new State(idRobot,new Location(locX, locY, locAlfa),new Velocity(0, 0),new Point(-1, -1));
+			
+			result.add(item);
+		}
+		
+		
+		
+		return  result;
 	}
 
 }
